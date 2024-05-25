d315914924a43dc31b084210a0b2816fcc6b05f4
Gottfried Herold <gottfried.herold@rub.de>
2024-05-26 01:44:18 +0200

    Doc.

--- market/runner.py
+++ market/runner.py
@@ -1,7 +1,7 @@
-from typing import Optional
+from typing import Optional, Dict
 from random import Random
 
-from .market import Cluster, Market
+from .market import Cluster, Market, Bid
 
 
 # Arbitrary constant. This is the default value in terms of MEV + issuance that
@@ -18,7 +18,7 @@ class Runner:
     """
 
     # Having an amount x of capital locked incurs a cost
-    # (essentially an opportunity cost from not being able to engage in DeFi)
+    # (essentially an opportunity cost from not being able to freely engage in DeFi)
     # of x * locked_capital_cost_per_epoch
     #
     # This value needs to be set to some reasonable value such as the expected return from staking.
@@ -29,17 +29,45 @@ class Runner:
     # NOTE2: capital locked is denominated in ETH, so the holder may still get value from
     # just holding it. Typically, this is a significant part of the reason why stakers stake in the first place.
     #
+    # For that reason, the ETH-denominated expected revenue from staking (over simply holding) ETH is reasonable
     locked_capital_cost_per_epoch: float
     market: Market
-    randomness_source: Random
-
+    randomness_source: Random  # defaults to Random()
     last_slot_proposer: Cluster
 
     def __init__(self, market: Market, *, locked_capital_cost_per_epoch: float,
-                 randomness_source: Random = None, initial_last_slot_proposer: Optional[Cluster] = None,
-                 initial_bids=None):
+                 randomness_source: Random = None, initial_last_slot_proposer: Optional[Cluster] = None):
+        """
+        initialize an instance of Runner, which holds the state of our simulation (including a market state).
+        Note that the separation of the Runner's state and the Market's state is somewhat fuzzy and not really
+        a "programming" question -- from a coding perspective, there is no reason to separate those.
+        It is rather a modelling one:
+        The Market class models what a market maker is able to choose and how the actual auctions work,
+        the Runner class models the (assumptions about the) dynamics of the participants' behaviour
+        and the general economy.
+
+        the parameters have the following meaning:
+        market is mandatory and should have a type (derived from) Market.
+        locked_capital_cost_per_epoch is used to set the per-epoch interest rate on capital cost.
+        (TODO: Using None should take a default value)
+        initial_last_slot_proposer is the initial last-slot proposer. If set to None, it is sampled randomly.
+        randomness_source is the source of randomness used in the simulation. None selects a default.
+        Note that when derandomizing everything, a randomness source must be provided to each of
+        the Runner class, the Market class and the Stake Distribution class separately.
+        We do not forward the randomness source from Runner to Market. This is not ideal, but simplifies the API.
+        Note that we do not deep-copy the randomness source, so one may use the same (stateful) randomness source for
+        each of these. This then behaves as a single randomness source for the whole simulation.
+        """
+
         self.market = market
-        self.locked_capital_cost_per_epoch = locked_capital_cost_per_epoch
+
+        # Will cause an error if we don't set a default value for locked_capital_cost_per_epoch
+        if hasattr(type(self), "locked_capital_cost_per_epoch"):
+            if locked_capital_cost_per_epoch is None:
+                raise ValueError("Must set locked_capital_cost_per_epoch")
+
+        if locked_capital_cost_per_epoch is not None:
+            self.locked_capital_cost_per_epoch = locked_capital_cost_per_epoch  #
 
         # randomness source:
         if randomness_source is None:
@@ -52,6 +80,8 @@ class Runner:
             self.last_slot_proposer = next(market.stake_dist.new_cluster_sampler(self.randomness_source))
         else:
             self.last_slot_proposer = initial_last_slot_proposer
+
+        assert initial_last_slot_proposer in self.market.participants
 
 
 
